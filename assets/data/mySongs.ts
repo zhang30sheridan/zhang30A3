c8ab178b367b058adc0210b0d86b5eecda273c56
zhang30sheridan <52877078+zhang30sheridan@users.noreply.github.com>
2019-07-29 11:26:53 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,8 @@
+import { SongInfo } from '../../app/songInfo';
+
+export const MYSONGS: SongInfo[] = [
+    {sname: 'I Dont Care', artist: 'Justin Bieber', genre:'Pop', year: '2019', picture:'./assets/images/1.png'},
+    {sname: 'You Need to Calm Down', artist: 'Taylor Swift', genre:'Synth-pop', year: '2019', picture:'./assets/images/2.png'},
+    {sname: 'Thank U, Next', artist: 'Ariana Grande', genre:'Pop', year: '2019', picture:'./assets/images/3.png'},
+    {sname: 'Bad Guy', artist: 'Billie Eilish', genre:'Pop', year: '2019', picture:'./assets/images/4.jpg'}
+];
